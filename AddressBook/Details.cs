1c4eeaec1465e72cacb88cd5eef53855feb4fc78
VivekG11 <vivekvk2903@gmail.com>
2021-07-03 22:44:43 +0530

    [Entering Details in the address  book]

---
+++ AddressBook/Details.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Details
+    {
+        /*.....................................
+         * Add necessary variables in class
+         * ................................*/
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public int MobileNumber { get; set; }
+            public int ZipCode { get; set; }
+
+            
+        }
+        /*...................................................
+         * creating a list to store details of people
+         * ...........................................*/
+        public static List<Person> people = new List<Person>();
+
+
+        public static void AddPerson()
+        {
+            Person person = new Person();
+
+            Console.WriteLine("Enter First Name : ");
+            person.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last NAme : ");
+            person.LastName = (Console.ReadLine());
+
+            Console.WriteLine("Enter Address");
+            person.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            person.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State");
+            person.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Mobile NUmber :");
+            person.MobileNumber =Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Enter Zip COde : ");
+            person.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+           
+
+        }
+        
+
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,9 +4,22 @@ namespace AddressBook
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book ................");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+
+                case 1:
+                    Details.AddPerson();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
